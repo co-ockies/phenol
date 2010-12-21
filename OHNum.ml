a6bee7e50bb6ca10322aabcc45ccc7633da03a62
co-ockies <nicolas.kirchner.4c2@laposte.net>
2010-12-21 23:29:53 +0100

    Ajout d'opérateurs à num

--- OHNum.ml
+++ OHNum.ml
@@ -4,10 +4,53 @@ type num = Ratio of rational | Undefined
 
 let fraction a b = Ratio (q a b);;
 let entier a = Ratio (z a);;
+
 let ( + ) a b = match (a, b) with
         | (Undefined, _) -> Undefined
         | (_, Undefined) -> Undefined
         | (Ratio x, Ratio y) -> Ratio (q_add x y);;
+
+let ( - ) a b = match (a, b) with
+        | (Undefined, _) -> Undefined
+        | (_, Undefined) -> Undefined
+        | (Ratio x, Ratio y) -> Ratio (q_sub x y);;
+
+let ( * ) a b = match (a, b) with
+        | (Undefined, _) -> Undefined
+        | (_, Undefined) -> Undefined
+        | (Ratio x, Ratio y) -> Ratio (q_mul x y);;
+
+let ( / ) a b = match (a, b) with
+        | (Undefined, _) -> Undefined
+        | (_, Undefined) -> Undefined
+        | (Ratio x, Ratio y) -> Ratio (q_div x y);;
+
+let (>= ) a b = match (a, b) with
+        | (Undefined, _) -> false
+        | (_, Undefined) -> false
+        | (Ratio x, Ratio y) -> q_sup_egal x y;;
+
+let ( = ) a b = match (a, b) with
+        | (Undefined, _) -> false
+        | (_, Undefined) -> false
+        | (Ratio x, Ratio y) -> q_egal x y;;
+
+let ( < ) a b = match (a, b) with
+        | (Undefined, _) -> false
+        | (_, Undefined) -> false
+        | (Ratio x, Ratio y) -> not (q_sup_egal x y);;
+
+let (<= ) a b = match (a, b) with
+        | (Undefined, _) -> false
+        | (_, Undefined) -> false
+        | (Ratio x, Ratio y) -> (q_egal x y) || (not (q_sup_egal x y));;
+
+let ( > ) a b = match (a, b) with
+        | (Undefined, _) -> false
+        | (_, Undefined) -> false
+        | (Ratio x, Ratio y) -> (q_sup_egal x y) && (not (q_egal x y));;
+
 let show a = match a with
         | Undefined -> "Undefined"
         | Ratio x -> q_show x;;
+
--- OHNum.mli
+++ OHNum.mli
@@ -4,4 +4,12 @@ type num = Ratio of rational | Undefined
 val fraction : int -> int -> num
 val entier : int -> num
 val ( + ) : num -> num -> num
+val ( - ) : num -> num -> num
+val ( * ) : num -> num -> num
+val ( / ) : num -> num -> num
+val ( > ) : num -> num -> bool
+val ( < ) : num -> num -> bool
+val ( = ) : num -> num -> bool
+val (>= ) : num -> num -> bool
+val (<= ) : num -> num -> bool
 val show : num -> string
